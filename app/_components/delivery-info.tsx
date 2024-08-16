af31bf863e26ca20c5196be886bbb89f3ab29154
Fabio Lopes <fabiolopex6@gmail.com>
2024-08-16 10:16:35 -0300

    feat: add restaurant page

---
+++ app/_components/delivery-info.tsx
@@ -0,0 +1,46 @@
+import { BikeIcon, TimerIcon } from "lucide-react";
+import { Card } from "./ui/card";
+import { formartCurrency } from "../_helpers/price";
+import { Restaurant } from "@prisma/client";
+
+interface DeliveryInfoProps {
+  restaurant: Pick<Restaurant, "deliveryFee" | "deliveryTimeMinutes">;
+}
+
+const DeliveryInfo = ({ restaurant }: DeliveryInfoProps) => {
+  return (
+    <>
+      <Card className="mt-6 flex justify-around py-3">
+        {/* CUSTO */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <BikeIcon size={14} />
+          </div>
+
+          {Number(restaurant.deliveryFee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formartCurrency(Number(restaurant.deliveryFee))}
+            </p>
+          ) : (
+            <p className="text-sm font-semibold">Grátis</p>
+          )}
+        </div>
+
+        {/* TEMPO */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <TimerIcon size={14} />
+          </div>
+
+          <div className="text-sm font-semibold">
+            {restaurant.deliveryTimeMinutes} Min
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default DeliveryInfo;
--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -8,15 +8,10 @@ import {
 import DiscoutBadge from "@/app/_components/discount-bage";
 import { Prisma } from "@prisma/client";
 import { Button } from "@/app/_components/ui/button";
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { useState } from "react";
-import { Card } from "@/app/_components/ui/card";
 import ProductList from "@/app/_components/product-list";
+import DeliveryInfo from "@/app/_components/delivery-info";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -94,48 +89,20 @@ const ProductDetails = ({
           </Button>
         </div>
       </div>
-      {/* DADOS DA ENTREGA */}
-      <Card className="mt-6 flex justify-around py-3">
-        {/* CUSTO */}
-        <div className="flex flex-col items-center">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            <span className="text-xs">Entrega</span>
-            <BikeIcon size={14} />
-          </div>
-
-          {Number(product.restaurant.deliveryFee) > 0 ? (
-            <p className="text-xs font-semibold">
-              {formartCurrency(Number(product.restaurant.deliveryFee))}
-            </p>
-          ) : (
-            <p className="text-sm font-semibold">Grátis</p>
-          )}
-        </div>
-
-        {/* TEMPO */}
-        <div className="flex flex-col items-center">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            <span className="text-xs">Entrega</span>
-            <TimerIcon size={14} />
-          </div>
-
-          <div className="text-sm font-semibold">
-            {product.restaurant.deliveryTimeMinutes} Min
-          </div>
-        </div>
-      </Card>
+      <div className="px5">
+        <DeliveryInfo restaurant={product.restaurant} />
+      </div>
       <div className="mt-6 space-y-3">
         <h3 className="font-semibold">Sobre</h3>
         <p className="text-sm text-muted-foreground">{product.description}</p>
       </div>
-      <div className="mb-4 mt-6 space-y-3 px-5">
+      <div className="mb-4 mt-6 space-y-3 px-2">
         <h3 className="font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
       <div className="mt-6 px-5">
         <Button className="w-full font-semibold">Adicionar a sacola</Button>
       </div>
-      ;
     </div>
   );
 };
